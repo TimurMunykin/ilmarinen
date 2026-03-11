abd548716863e3d34683693ca73c3feac74f1530
Timur Munykin <timur.munykin@gmail.com>
2026-03-11 20:19:27 +0100

    feat(template): add API skeleton with Prisma and NestJS bootstrap

---
+++ template/apps/api/src/main.ts
@@ -0,0 +1,24 @@
+import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api');
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+
+  const config = new DocumentBuilder()
+    .setTitle('{{APP_NAME}} API')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
+  await app.listen(3001);
+  console.log('API running on http://localhost:3001');
+}
+bootstrap();
